83e69ae444974a2cc080a51fd806ff75631fa32a
casaxmore <casaxmore@gmail.com>
2023-01-11 23:36:04 +0100

    Subida final

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interface/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje =
+    {
+      nombre: 'Chiquito',
+      poder: 666
+    }
+
+    constructor(){}
+}
